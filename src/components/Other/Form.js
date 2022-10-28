30b986616e678cb77974262218d0c1f02e422fd9
jenjarus <jenja-oskol@yandex.ru>
2022-10-28 15:04:49 +0300

    Форма + валидация

--- src/components/Other/Form.js
+++ src/components/Other/Form.js
@@ -2,53 +2,82 @@ import React, {useState} from "react";
 
 const Form = ({nameForm = '', textBtn = 'Отправить', successText, children}) => {
     const [success, setSuccess] = useState(false);
-    const [errorValid, setErrorValid] = useState(false);
-    const [formValue, setFormValue] = useState({});
-    successText = successText ? successText : (
-        <div className="form-success">
-            <div className="form-success__title">Сообщение успешно отправленно</div>
-            <div className="form-success__text">Мы свяжемся с вами в ближайшее время.</div>
-        </div>);
+    const [errorForm, setErrorForm] = useState(false);
+    const [dataForm, setDataForm] = useState({});
+    successText = successText ? successText : <SuccessText />;
 
-    const handlerSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        if(!errorValid) {
+        if (validateForm(e.target)) {
+            setDataForm({});
             setSuccess(true);
-            console.log(formValue);
         }
     };
 
-    /*const validInput = (that) => {
-        if(required) {
-            console.log(that.value.trim().length);
-            if(nameInput === 'phone' && that.value.trim().length !== 18) {
-                setErrorValid(true);
-                setError(true);
-                //return (<span className="form-input__error-text">Заполните поле {that.value.length}</span>)
-            } else {
-                setErrorValid(false);
-                setError(false);
-                //return (<span className="form-input__error-text">Заполните поле</span>)
+    const validateForm = (target) => {
+        let formData = {};
+        let errors = {};
+        let cloneDataForm = dataForm.length ? dataForm : formData;
+        let isValid = true;
+
+        if (!dataForm.length) {
+            for (let i = 0; i < target.length; i++) {
+                if (target.elements[i].type !== 'submit') {
+                    formData[target.elements[i].getAttribute("name")] = {
+                        name: target.elements[i].name,
+                        value: target.elements[i].value,
+                        required: target.elements[i].required,
+                    };
+                }
             }
-        } else {
-            setErrorValid(false);
-            setError(false);
         }
-    };*/
+
+        for (let key in cloneDataForm) {
+            if (cloneDataForm[key].required) {
+                if (cloneDataForm[key].value.length <= 0) {
+                    isValid = false;
+                    errors[cloneDataForm[key].name] = {
+                        name: cloneDataForm[key].name,
+                        error: true,
+                    };
+                } else if (cloneDataForm[key].name === 'phone' && cloneDataForm[key].value.length !== 18) {
+                    isValid = false;
+                    errors[cloneDataForm[key].name] = {
+                        name: cloneDataForm[key].name,
+                        error: true,
+                        customText: "Заполните поле полностью",
+                    };
+                }
+            }
+        }
+
+        setErrorForm(errors);
+
+        return isValid;
+    };
 
     const childrenWithProps = React.Children.map(children, (child) => {
         if (React.isValidElement(child)) {
-            return React.cloneElement(child, {setFormValue, formValue, nameForm, setErrorValid});
+            return React.cloneElement(child, {setDataForm, dataForm, nameForm, errorForm});
         }
         return child;
     });
 
-    const renderSuccessInfo = () => {
-        if(success) {
+    function SuccessText() {
+        return (
+            <div className="form-success">
+                <div className="form-success__title">Сообщение успешно отправленно</div>
+                <div className="form-success__text">Мы свяжемся с вами в ближайшее время.</div>
+            </div>
+        )
+    }
+
+    const RenderContent = () => {
+        if (success) {
             return successText
         } else {
             return (
-                <form action="#" method="post" className={nameForm + "-form"} onSubmit={handlerSubmit}>
+                <form action="#" method="post" className={nameForm + "-form"} onSubmit={handleSubmit} noValidate>
                     {childrenWithProps}
                     <div className={`form-submit ${nameForm}--submit`}>
                         <button type="submit" className={`btn ${nameForm}-btn`}>{textBtn}</button>
@@ -58,7 +87,7 @@ const Form = ({nameForm = '', textBtn = 'Отправить', successText, child
         }
     };
 
-    return renderSuccessInfo();
+    return RenderContent();
 
 };
 
--- src/components/Other/FormInput.js
+++ src/components/Other/FormInput.js
@@ -1,43 +1,35 @@
 import {useState} from "react";
 import InputMask from 'react-input-mask';
 
-const FormInput = ({nameInput = '', required = false, children, setFormValue, formValue, nameForm, setErrorValid}) => {
+const FormInput = ({nameInput = '', required = false, children, setDataForm, dataForm, nameForm, errorForm}) => {
     const [focused, setFocused] = useState(false);
-    const [error, setError] = useState(false);
     const focusedClass = focused ? "focused" : '';
-    const errorClass = error ? " form-input__error" : '';
+    const error = errorForm[nameInput];
+    const errorClass = errorForm[nameInput] ? " form-input__error" : '';
     const nameId = nameForm + "-" + nameInput;
 
-    /*const validInput = (that) => {
-        if(required) {
-            console.log(that.value.trim().length);
-            if(nameInput === 'phone' && that.value.trim().length !== 18) {
-                setErrorValid(true);
-                setError(true);
-                //return (<span className="form-input__error-text">Заполните поле {that.value.length}</span>)
-            } else {
-                setErrorValid(false);
-                setError(false);
-                //return (<span className="form-input__error-text">Заполните поле</span>)
-            }
-        } else {
-            setErrorValid(false);
-            setError(false);
-        }
-    };*/
+
+    const handleChange = (e) => {
+        let newDataForm = dataForm;
+        newDataForm[e.target.name] = {
+            name: e.target.name,
+            value: e.target.value,
+            required: e.target.required,
+        };
+
+        setDataForm((prevState) => ({
+            ...prevState,
+            newDataForm}));
+
+        setFocused(!!e.target.value.length);
+    };
 
     const renderInput = () => {
         if(nameInput === 'phone') {
-                return <InputMask type="text" mask="+7 (999) 999-99-99" className={focusedClass+errorClass} name={nameInput} id={nameId} required={required} onChange={handlerChange} />;
+                return <InputMask type="text" mask="+7 (999) 999-99-99" className={errorClass + focusedClass} name={nameInput} id={nameId} required={required} onChange={handleChange} />;
         } else {
-            return <input type="text" className={focusedClass} name={nameInput} id={nameId} required={required} onChange={handlerChange} />
+            return <input type="text" className={errorClass + focusedClass} name={nameInput} id={nameId} required={required} onChange={handleChange} />
         }
-    };
-
-    const handlerChange = (e) => {
-        setFocused(!!e.target.value.length);
-        /*validInput(e.target);*/
-        setFormValue({...formValue, [e.target.name]: e.target.value})
     };
 
     return (
@@ -45,7 +37,7 @@ const FormInput = ({nameInput = '', required = false, children, setFormValue, fo
             <div className="form-input">
                 {renderInput()}
                 {children && <label htmlFor={nameId}>{children}{required && ' *'}</label>}
-                {error && <span className="form-input__error-text">Заполните поле</span>}
+                {error && <span className="form-input__error-text">{error.customText ? error.customText : 'Заполните поле'}</span>}
             </div>
         </>
     )
